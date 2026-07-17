f7607a9f951efb38994845cc3121cb2750e751ec
Mendieta Hernandez <gmendiet@ndc.nasa.gov>
2026-07-17 15:40:38 -0700

    Deleted the different implementation for vectors bc logic was getting too complex. Continued with the implementation of the parse() function. I still need to define implemented functions in their respective header file. And I need to do some testing and debugging

--- src/cargs.c
+++ src/cargs.c
@@ -64,76 +64,167 @@ bool intInInts(int n, int* ints, size_t size) {
     return false;
 }
 
-Arg* parse(char** argv, int argc, ArgDef* definitions, size_t size) {
-    size_t i = 0;
+bool isArgsExist(Arg* args, size_t argsS, const Arg* arg) {
+    size_t left = 0;
+    size_t right = argsS - 1;
+
+    while (left <= right) {
+        if (strcmp(args[left].name, arg->name) == 0) return true;
+        if (strcmp(args[right].name, arg->name) == 0) return true;
+        left++;
+        right--;
+    }
+
+    return false;
+}
+
+// bool valueIsArgDef(ArgDef* definitions, size_t defsSize, const char* value) {
+//     size_t left = 0;
+//     size_t right = defsSize - 1;
+//
+//     while (left <= right) {
+//         if (strcmp(definitions[left].name, value) == 0) return true;
+//         if (strcmp(definitions[right].name, value) == 0) return true;
+//         left++;
+//         right--;
+//     }
+//
+//     return false;
+// }
+
+Arg* parse(char** argv, int argc, ArgDef* definitions, size_t defsSize) {
+    // skipping the first argument since its always the program name
+    size_t i = 1;
+
+    Arg* args = malloc(sizeof(Arg) * defsSize);
+    size_t argsP = 0;
+
+    if (args == NULL) {
+        fprintf(stderr, "Could not allocate memory for arguments\n");
+        return NULL;
+    }
 
     while (i < argc) {
         char* arg = argv[i];
 
+        if (strcmp(arg, "--") == 0) {
+            i += 1;
+            continue;
+        }
+
         // looking for its definition
-        size_t idx = findArgDef(arg, definitions, size);
+        size_t idx = getArgDefWithName(arg, definitions, defsSize);
 
         if (idx == -1) {
-            fprintf(stderr, "Unexpected argument given: `%s`\n", argv[idx]);
-            exit(EXIT_FAILURE);
+            fprintf(stderr, "Unexpected argument given: `%s`\n", arg);
+            return NULL;
         }
         
         switch (definitions[i].type) {
-            case STRING:
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Please provide a string value to: `%s`\n", arg);
-                exit(EXIT_FAILURE);
-            }
-            char* argValue = argv[i + 1];
+            case STRING: {
+                printf("Entering the STRING branch\n");
 
-            Arg a = {
-                .name = arg,
-                .value = {
-                    .type = STRING,
-                    .variant.s = argValue
+                if (i + 1 >= argc) {
+                    fprintf(stderr, "Please provide a string value to: `%s`\n", arg);
+                    return NULL;
                 }
-            };
-            // do something with `a` here
+                char* argValue = argv[i + 1];
 
-            i += 2;
-            break;
-            case BOOLEAN:
-            Arg a = {
-                .name = arg,
-                .value = {
-                    .type = BOOLEAN,
-                    .variant.b = true;
+                printf("Acquired the arguments value: %s\n", argValue);
+
+                bool isNotAnArgument = getArgDefWithName(argValue, definitions, defsSize) != -1;
+                bool hasPrecedingDecouplingToken = i != 0 && strcmp(argv[i - 1], "--") != 0;
+
+                if (isNotAnArgument && hasPrecedingDecouplingToken) {
+                    // means that the taken value is actually a flag
+                    fprintf(
+                        stderr,
+                        "Please provide a valid value for `%s`, not `%s`. If this is what you wanted, preceed the token with `--` as a token\n",
+                        arg,
+                        argValue
+                    );
+                    return NULL;
                 }
-            }
-            // do something with `a` here
 
-            i += 1;
-            break;
-            case NUMERICAL:
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Please provide a numerical value to: `%s`\n", arg);
-                exit(EXIT_FAILURE);
-            }
-            char* argValue = argv[i + 1];
-            char* end;
+                Arg a = {
+                    .name = arg,
+                    .value = {
+                        .type = STRING,
+                        .variant.s = argValue
+                    }
+                };
 
-            float floatArgValue = strtof(argValue, &end);
-            if (end == argValue) {
-                fprintf(stderr, "Error parsing numerical value: `%f`\n", argValue);
-                exit(EXIT_FAILURE);
-            }
+                printf("Constructed the argument object");
 
-            Arg a = {
-                .name = arg,
-                .value = {
-                    .type = BOOLEAN,
-                    .variant.s = argValue
+                if (!isArgsExist(args, argsP, &a)) {
+                    args[argsP++] = a;
                 }
-            };
-            // do something with `a` here
 
-            i += 2;
-            break;
+                i += 2;
+                printf("Skipped the i pointer two times. Should be continuing");
+                break;
+            }
+            case BOOLEAN: {
+                Arg a = {
+                    .name = arg,
+                    .value = {
+                        .type = BOOLEAN,
+                        .variant.b = true
+                    }
+                };
+
+                if (!isArgsExist(args, argsP, &a)) {
+                    args[argsP++] = a;
+                }
+
+                i += 1;
+                break;
+            }
+            case NUMERICAL: {
+                if (i + 1 >= argc) {
+                    fprintf(stderr, "Please provide a numerical value to: `%s`\n", arg);
+                    return NULL;
+                }
+                char* argValue = argv[i + 1];
+                char* end;
+
+                float floatArgValue = strtof(argValue, &end);
+                if (end == argValue) {
+                    fprintf(stderr, "Error parsing numerical value: `%f`\n", argValue);
+                    return NULL;
+                }
+
+                if ((getArgDefWithName(argValue, definitions, defsSize) != -1) && (i != 0 && strcmp(argv[i - 1], "--") != 0)) {
+                    // means that the taken value is actually a flag
+                    fprintf(
+                        stderr,
+                        "Please provide a valid value for `%s`, not `%d`. If this is what you wanted, preceed the token with `--` as a token\n",
+                        arg,
+                        floatArgValue
+                    );
+                    return NULL;
+                }
+
+                Arg a = {
+                    .name = arg,
+                    .value = {
+                        .type = BOOLEAN,
+                        .variant.s = argValue
+                    }
+                };
+
+                if (!isArgsExist(args, argsP, &a)) {
+                    args[argsP++] = a;
+                }
+
+                i += 2;
+                break;
+            }
         }
+    }
+
+    // looking for argument definitions that were deemed as not optional
+    for (size_t i = 0; i < defsSize; ++i) {
+        // if (!definitions[i].optional && getArgDefWithName())
     }
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <cargs/cargs.h>
 
-int main() {
-    ArgDef arg = newArg("Gabriel", STRING, false);
+int main(int argc, char** argv) {
+    ArgDef definitions[1] = {
+        newArgDef("--name", STRING, false)
+    };
+    Arg* args = parse(argv, argc, definitions, 1);
+
+    Arg arg = args[0];
+
+    printf("Argument value for %s: %s", arg.name, arg.value.variant.s);
+    // free(args);
     return 0;
 }
